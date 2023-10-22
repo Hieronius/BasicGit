e9c30d3b26ec1d18bbd2e12717bdbc000f6293d3
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-22 06:36:36 +0300

    commit message goes here

---
+++ BasicGit/BasicGit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BasicGit
+//
+//  Created by Арсентий Халимовский on 22.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
